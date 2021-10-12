6af425e3b3efc6e4fd374c23aa89f4531e6571bf
Zach Salehe <z.salehe@mail.utoronto.ca>
2021-10-11 20:56:31 -0400

    user class created

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,6 +1,95 @@
 import java.util.HashMap;
+import java.util.ArrayList;
 
 public class User {
-    private HashMap<String, String> likes;
-    private HashMap<String, String> seenUsers;
+    private String id;
+    private HashMap<String, String> loginInfo;
+    private HashMap<String, String> personalStats;
+    private String fightingStyle;
+    private String biography;
+    private String controversialOpinions;
+    private ArrayList<User> seenUsers;
+    private ArrayList<User> likes;
+    private ArrayList<User> matches;
+
+    public User(String id, HashMap<String, String> loginInfo, HashMap<String, String> personalStats,
+                String fightingStyle, String biography, String controversialOpinions){
+        this.id = id;
+        this.loginInfo = loginInfo;
+        this.personalStats = personalStats;
+        this.fightingStyle = fightingStyle;
+        this.biography = biography;
+        this.controversialOpinions = controversialOpinions;
+        this.seenUsers = new ArrayList<>();
+        this.likes = new ArrayList<>();
+        this.matches = new ArrayList<>();
+    }
+
+    public String getId(){
+        return this.id;
+    }
+
+    public HashMap<String, String> getLoginInfo(){
+        return this.loginInfo;
+    }
+
+    public HashMap<String, String> getPersonalStats(){
+        return this.personalStats;
+    }
+
+    public String getFightingStyle(){
+        return this.fightingStyle;
+    }
+
+    public String getBiography(){
+        return this.biography;
+    }
+
+    public String getControversialOpinions(){
+        return this.controversialOpinions;
+    }
+
+    public ArrayList<User> getSeenUsers(){
+        return this.seenUsers;
+    }
+
+    public ArrayList<User> getLikes(){
+        return this.likes;
+    }
+
+    public ArrayList<User> getMatches(){
+        return this.matches;
+    }
+
+    public void setLoginInfo(HashMap<String, String> loginInfo){
+        this.loginInfo = loginInfo;
+    }
+
+    public void setPersonalStats(HashMap<String, String> personalStats){
+        this.personalStats = personalStats;
+    }
+
+    public void setFightingStyle(String fightingStyle){
+        this.fightingStyle = fightingStyle;
+    }
+
+    public void setBiography(String biography){
+        this.biography = biography;
+    }
+
+    public void setControversialOpinions(String controversialOpinions){
+        this.controversialOpinions = controversialOpinions;
+    }
+
+    public void addSeenUser(User user){
+        this.seenUsers.add(user);
+    }
+
+    public void addLike(User user){
+        this.likes.add(user);
+    }
+
+    public void addMatch(User user){
+        this.matches.add(user);
+    }
 }
