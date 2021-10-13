e549d363ecc5f7cc6f9c92b06361d5c7e7d366b3
Josh <jdpuglielli@gmail.com>
2021-10-13 18:39:27 -0400

    Suggestion in Brawlr.main, and segregated Brawlr from swipe functionality.

--- src/main/java/Brawlr.java
+++ src/main/java/Brawlr.java
@@ -3,13 +3,9 @@ public class Brawlr {
 
     public static void main(String[] args) {
         // TODO: 10/11/2021
-    }
-
-    public static void swipeRight(User user1, User user2){
-        UserManager.addLike(user1, user2);
-    }
-
-    public static void swipeLeft(User user1, User user2){
-        UserManager.addSeenUser(user1, user2);
+        //Just for the initial runthrough, should we do the following?
+        //UserDatabase users = new UserDatabase
+        //users.add(...)
+        //
     }
 }
