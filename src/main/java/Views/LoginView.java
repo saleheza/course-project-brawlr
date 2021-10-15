a59bfe4e5ddac907933d21d43eed336f95aa913b
Josh <jdpuglielli@gmail.com>
2021-10-15 15:40:04 -0400

    Minor Edits

--- src/main/java/Views/LoginView.java
+++ src/main/java/Views/LoginView.java
@@ -2,12 +2,4 @@ package Views;
 import java.util.*;
 
 public class LoginView{
-    public static void displayLogin(){
-        Scanner in = new Scanner(System.in);
-        System.out.println("Please enter your username:");
-        String userId = in.nextLine();
-        System.out.println("Please enter your password:");
-        String pass = in.nextLine();
-        //brawlr.brawlr.InputManager call for existing brawlr.User
-    }
 }
